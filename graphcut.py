5ff1653ac4c0caedabfb5ce91e1dcd28184ce8cb
Lalit <2024csm1011@iitrpr.ac.in>
2024-11-07 11:03:57 +0530

    CHANGED SOME PORTED CODE

--- graphcut.py
+++ graphcut.py
@@ -1,4 +1,3 @@
-from __future__ import division
 import cv2
 import numpy as np
 import os
@@ -8,10 +7,12 @@ from augmentingPath import augmentingPath
 from pushRelabel import pushRelabel
 from boykovKolmogorov import boykovKolmogorov
 
+# np.set_printoptions(threshold=np.inf)
 graphCutAlgo = {"ap": augmentingPath, 
                 "pr": pushRelabel, 
                 "bk": boykovKolmogorov}
 SIGMA = 30
+# LAMBDA = 1
 OBJCOLOR, BKGCOLOR = (0, 0, 255), (0, 255, 0)
 OBJCODE, BKGCODE = 1, 2
 OBJ, BKG = "OBJ", "BKG"
@@ -21,6 +22,7 @@ CUTCOLOR = (0, 0, 255)
 SOURCE, SINK = -2, -1
 SF = 10
 LOADSEEDS = False
+# drawing = False
 
 def show_image(image):
     windowname = "Segmentation"
@@ -51,10 +53,10 @@ def plantSeed(image):
             drawing = False
 
     def paintSeeds(pixelType):
-        print("Planting", pixelType, "seeds")
+        print(f"Planting {pixelType} seeds")
         global drawing
         drawing = False
-        windowname = "Plant " + pixelType + " seeds"
+        windowname = f"Plant {pixelType} seeds"
         cv2.namedWindow(windowname, cv2.WINDOW_AUTOSIZE)
         cv2.setMouseCallback(windowname, onMouse, pixelType)
         while True:
@@ -68,7 +70,7 @@ def plantSeed(image):
     image = cv2.resize(image, (0, 0), fx=SF, fy=SF)
 
     radius = 10
-    thickness = -1  # fill the whole circle
+    thickness = -1 # fill the whole circle
     global drawing
     drawing = False
     
@@ -76,8 +78,10 @@ def plantSeed(image):
     paintSeeds(BKG)
     return seeds, image
 
+
+# Large when ip - iq < sigma, and small otherwise
 def boundaryPenalty(ip, iq):
-    bp = 100 * exp(-pow(int(ip) - int(iq), 2) / (2 * pow(SIGMA, 2)))
+    bp = 100 * exp(- pow(int(ip) - int(iq), 2) / (2 * pow(SIGMA, 2)))
     return bp
 
 def buildGraph(image):
@@ -94,12 +98,12 @@ def makeNLinks(graph, image):
     for i in range(r):
         for j in range(c):
             x = i * c + j
-            if i + 1 < r:  # pixel below
+            if i + 1 < r: # pixel below
                 y = (i + 1) * c + j
                 bp = boundaryPenalty(image[i][j], image[i + 1][j])
                 graph[x][y] = graph[y][x] = bp
                 K = max(K, bp)
-            if j + 1 < c:  # pixel to the right
+            if j + 1 < c: # pixel to the right
                 y = i * c + j + 1
                 bp = boundaryPenalty(image[i][j], image[i][j + 1])
                 graph[x][y] = graph[y][x] = bp
@@ -112,25 +116,64 @@ def makeTLinks(graph, seeds, K):
     for i in range(r):
         for j in range(c):
             x = i * c + j
-            if seeds[i][j][0] == OBJCODE:  # Object seed
-                graph[0][x] = K  # Source to object seed
-            elif seeds[i][j][0] == BKGCODE:  # Background seed
-                graph[x][1] = K  # Background seed to sink
+            if seeds[i][j] == OBJCODE:
+                # graph[x][source] = K
+                graph[SOURCE][x] = K
+            elif seeds[i][j] == BKGCODE:
+                graph[x][SINK] = K
+                # graph[sink][x] = K
+            # else:
+            #     graph[x][source] = LAMBDA * regionalPenalty(image[i][j], BKG)
+            #     graph[x][sink]   = LAMBDA * regionalPenalty(image[i][j], OBJ)
 
-    return graph
+def displayCut(image, cuts):
+    def colorPixel(i, j):
+        image[i][j] = CUTCOLOR
 
-def main():
-    parser = argparse.ArgumentParser(description='Graph Cut Segmentation')
-    parser.add_argument('image', type=str, help='Path to the input image')
-    args = parser.parse_args()
+    r, c = image.shape
+    image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+    for c in cuts:
+        if c[0] != SOURCE and c[0] != SINK and c[1] != SOURCE and c[1] != SINK:
+            colorPixel(c[0] // r, c[0] % r)
+            colorPixel(c[1] // r, c[1] % r)
+    return image
 
-    image = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
-    if image is None:
-        print("Error: Could not read the image.")
-        return
-
+def imageSegmentation(imagefile, size=(30, 30), algo="ff"):
+    pathname = os.path.splitext(imagefile)[0]
+    image = cv2.imread(imagefile, cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(image, size)
     graph, seededImage = buildGraph(image)
-    # Further processing can be done here, such as running the graph cut algorithm
+    cv2.imwrite(pathname + "seeded.jpg", seededImage)
+
+    global SOURCE, SINK
+    SOURCE += len(graph) 
+    SINK   += len(graph)
+    
+    cuts = graphCutAlgo[algo](graph, SOURCE, SINK)
+    print("cuts:")
+    print(cuts)
+    image = displayCut(image, cuts)
+    image = cv2.resize(image, (0, 0), fx=SF, fy=SF)
+    show_image(image)
+    savename = pathname + "cut.jpg"
+    cv2.imwrite(savename, image)
+    print(f"Saved image as {savename}")
+
+def parseArgs():
+    def algorithm(string):
+        if string in graphCutAlgo:
+            return string
+        raise argparse.ArgumentTypeError(
+            f"Algorithm should be one of the following: {', '.join(graphCutAlgo.keys())}")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("imagefile")
+    parser.add_argument("--size", "-s", 
+                        default=60, type=int,
+                        help="Defaults to 60x60")
+    parser.add_argument("--algo", "-a", default="ap", type=algorithm)
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    main() 
+    args = parseArgs()
+    imageSegmentation(args.imagefile, (args.size, args.size), args.algo)
